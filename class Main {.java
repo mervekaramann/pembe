a4b927c9fa8a9784698602162868cddc65aeb663
mervekaramann <mervekaraman1@stu.aydin.edu.tr>
2025-07-18 21:42:18 +0300

    ?

--- class Main {.java
+++ class Main {.java
@@ -2,6 +2,7 @@ class Main {
     public static void main(String[] args) {
         System.out.println("BACICORE");
         System.out.println("bacılarımmm <3");
-System.out.println("BACICORE ıs eşşek");
+        System.out.println("BACICORE ıs eşşek");
+        System.out.println("neden olmuyor?");
     }
 }
