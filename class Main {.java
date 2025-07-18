fb31d9ce082ef47e507f9d9c67fb9e290884eaa0
sserhatpolatli <suleymanserhatpolatli@gmail.com>
2025-07-18 21:25:38 +0300

    aaa

--- class Main {.java
+++ class Main {.java
@@ -1,5 +1,9 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("BACICORE ıs eşşek");
+
+
+        System.out.println("BACICORE");
+        System.out.println("bacılarımmm <3");
+
     }
 }
